75c1cc9e39e72d5588416705e7e4dfe5ee77c9c1
Jordan Gaslonde <J.Gaslonde@outlook.com>
2020-11-09 12:59:52 +0000

    Blazor lifecycle

---
+++ BlazorProducts.Client/Componets/Home.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorProducts.Client.Componets
+{
+    public partial class Home
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> AdditionalAttributes { get; set; }
+
+        [CascadingParameter(Name = "HeadingColour")]
+        public string Colour { get; set; }
+
+        [Parameter]
+        public RenderFragment VisitShopContent { get; set; }            
+    }
+}
